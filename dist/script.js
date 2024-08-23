b2895186b454261069c3e13ba28d86a3cf395d3e
Xpensive-tech <divinep091@gmail.com>
2024-08-23 11:43:53 +0100

    New commits

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,21 @@
-console.log('Hello world')
+// console.log('Hello world')
+
+// For tablet and ipad
+
+var e = window.matchMedia('(min-width:481px) and (max-width: 768px)');
+myChecked(e);
+
+e.addEventListener('change', function(){
+    function myChecked(e) {
+        if (e.matches) { // If media query matches
+          document.body.style.backgroundColor = "yellow";
+        } else {
+         document.body.style.backgroundColor = "pink";
+        }
+      }
+});
+   
+   
+       
+   
+
